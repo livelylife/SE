04bee52770f3773a772e3ed12726cbdf3c5bcf29
unknown <w2370357693@163.com>
2020-06-18 19:41:56 +0800

    添加用户管理员界面

--- SE/src/main/java/com/SE/servlet/myServlet.java
+++ SE/src/main/java/com/SE/servlet/myServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "myServlet",value = "/myServlet")
+@WebServlet(name = "myServlet", value = "/myServlet")
 public class myServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -16,6 +16,6 @@ public class myServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doPost(request,response);
+        doPost(request, response);
     }
 }
